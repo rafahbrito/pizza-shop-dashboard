fc80e9163efdd22963c0748c0c0b5e80e5b633a9
Rafael Brito <henrique8264@gmail.com>
2024-09-09 19:31:55 -0300

    feat(Theme): :sparkles: Adiciona temas na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/sonner'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="system" storageKey="pizza.shop">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
