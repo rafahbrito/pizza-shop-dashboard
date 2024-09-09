2b51c28aba35eeb6066839fcf85b36269d7694f3
Rafael Brito <henrique8264@gmail.com>
2024-09-09 18:23:52 -0300

    feat(Layout): :sparkles: Melhora os estilos dos Layouts da aplicação

--- src/pages/layouts/app.tsx
+++ src/pages/layouts/app.tsx
@@ -2,9 +2,9 @@ import { Outlet } from 'react-router-dom'
 
 export function AppLayout() {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col antialiased">
       <h1>Header</h1>
-      <main>
+      <main className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </main>
     </div>
--- src/pages/layouts/auth.tsx
+++ src/pages/layouts/auth.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2">
+    <div className="grid min-h-screen grid-cols-2 antialiased">
       <div className="flex h-full flex-col items-center justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-1 text-lg text-foreground">
           <Pizza className="h5 w-5" />
