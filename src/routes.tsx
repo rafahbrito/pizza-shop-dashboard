a927501e6021bd98d8581fda4f5a555c33b6b7dd
Rafael Brito <henrique8264@gmail.com>
2024-09-17 17:50:41 -0300

    feat(Layout): :sparkles: Adiciona Pagina de Not Found

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,11 +6,13 @@ import { SignIn } from '@/pages/auth/sign-in'
 import { SignUp } from '@/pages/auth/sign-up'
 import { AppLayout } from '@/pages/layouts/app'
 import { AuthLayout } from '@/pages/layouts/auth'
+import { NotFoundPage } from '@/pages/NotFoundPage'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFoundPage />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
